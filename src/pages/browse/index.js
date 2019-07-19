b9c3011f0b9716f60303b0ac6d9deb248b2fa48e
Arli <arli.machado@gmail.com>
2019-07-19 16:18:49 -0300

    Definindo pagina de navegação

---
+++ src/pages/browse/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { Container, Title, List, Playlist } from "./styles";
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Silverchair_-_Diorama.jpg/220px-Silverchair_-_Diorama.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Silverchair_-_Diorama.jpg/220px-Silverchair_-_Diorama.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Silverchair_-_Diorama.jpg/220px-Silverchair_-_Diorama.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Silverchair_-_Diorama.jpg/220px-Silverchair_-_Diorama.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+    </List>
+  </Container>
+);
+export default Browse;
