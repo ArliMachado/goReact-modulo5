0d3cc7e3e3727d2c8269f9d3df406042340b73ce
Arli <arli.machado@gmail.com>
2019-07-23 14:17:53 -0300

    Alterando a cor da musica quando selecionado

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -6,7 +6,7 @@ import { bindActionCreators } from "redux";
 import { Creators as PlaylistDetailsAction } from "../../store/ducks/playlistDetails";
 import { Creators as PlayerAction } from "../../store/ducks/player";
 
-import { Container, Header, SongList } from "./styles";
+import { Container, Header, SongList, SongItem } from "./styles";
 
 import ClockIcon from "../../assets/images/clock.svg";
 import PlusIcon from "../../assets/images/plus.svg";
@@ -37,8 +37,16 @@ class Playlist extends Component {
       }),
       loading: PropTypes.bool
     }).isRequired,
-    loadSong: PropTypes.func.isRequired
+    loadSong: PropTypes.func.isRequired,
+    currentSong: PropTypes.shape({
+      id: PropTypes.number
+    }).isRequired
   };
+
+  state = {
+    selectedSong: null
+  };
+
   componentDidMount() {
     this.loadPlaylistDetails();
   }
@@ -89,9 +97,15 @@ class Playlist extends Component {
               </tr>
             ) : (
               playlist.songs.map(song => (
-                <tr
+                <SongItem
                   key={song.id}
+                  onClick={() => this.setState({ selectedSong: song.id })}
                   onDoubleClick={() => this.props.loadSong(song)}
+                  selected={this.state.selectedSong === song.id}
+                  playing={
+                    this.props.currentSong &&
+                    this.props.currentSong.id === song.id
+                  }
                 >
                   <td>
                     <img src={PlusIcon} alt="Adicionar" />
@@ -100,7 +114,7 @@ class Playlist extends Component {
                   <td>{song.author}</td>
                   <td>{song.album}</td>
                   <td>3:51</td>
-                </tr>
+                </SongItem>
               ))
             )}
           </tbody>
@@ -121,7 +135,8 @@ class Playlist extends Component {
 }
 
 const mapStateToProps = state => ({
-  playlistDetails: state.playlistDetails
+  playlistDetails: state.playlistDetails,
+  currentSong: state.player.currentSong
 });
 
 const mapDispatchToProps = dispatch =>
