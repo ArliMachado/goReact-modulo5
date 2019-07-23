30a9e00dba731bc94aa6a6175ae651c3188a8134
Arli <arli.machado@gmail.com>
2019-07-23 11:10:41 -0300

    Refatorando o componente de tratamento de erro

--- src/components/ErrorBox/index.js
+++ src/components/ErrorBox/index.js
@@ -1,14 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as ErrorActions } from "../../store/ducks/error";
+
 import CloseIcon from "../../assets/images/close.svg";
 
 import { Container } from "./styles";
-const ErrorBox = () => (
-  <Container>
-    <p>Mensagem de erro</p>
-    <button>
-      <img src={CloseIcon} alt="Fechar" />
-    </button>
-  </Container>
-);
 
-export default ErrorBox;
+const ErrorBox = ({ error: { message, visible }, hideError }) =>
+  visible && (
+    <Container>
+      <p>{message}</p>
+      <button onClick={hideError}>
+        <img src={CloseIcon} alt="Fechar" />
+      </button>
+    </Container>
+  );
+
+ErrorBox.propTypes = {
+  error: PropTypes.shape({
+    message: PropTypes.string,
+    visible: PropTypes.bool
+  }).isRequired,
+  hideError: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  error: state.error
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(ErrorActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ErrorBox);
--- src/styles/global.js
+++ src/styles/global.js
@@ -22,6 +22,10 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Montserrat', sans-serif;
     color: ${colors.white};
   }
+
+  button {
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyle;
