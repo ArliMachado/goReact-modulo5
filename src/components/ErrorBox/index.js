d685d96cc9e1c9ff07957d6df8ee1e2dc284129d
Arli Machado <arli.machado@gmail.com>
2019-07-22 23:45:01 -0300

    Definindo componente de erro

---
+++ src/components/ErrorBox/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CloseIcon from "../../assets/images/close.svg";
+
+import { Container } from "./styles";
+const ErrorBox = () => (
+  <Container>
+    <p>Mensagem de erro</p>
+    <button>
+      <img src={CloseIcon} alt="Fechar" />
+    </button>
+  </Container>
+);
+
+export default ErrorBox;
